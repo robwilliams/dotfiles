a2a2497166e8bee1b95689cad0dbc940f0b7b5f5
Robert Williams <rob@r-williams.com>
2012-05-12 15:07:14 +0100

    Added vim configuration.

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-for file in tmux.conf zshrc gitignore_global
+for file in tmux.conf zshrc gitignore_global vim gvimrc vimrc
 do
   echo "Linking $file to ~/.$file..";
   ln -sf ~/dotfiles/$file ~/.$file;
 done
-
