edcadf2832dab7f97b94a019f978943cd11647f0
Robert Williams <rob@r-williams.com>
2012-05-12 17:28:24 +0100

    Added gitconfig.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in tmux.conf zshrc gitignore_global vim gvimrc vimrc ssh/config
+for file in tmux.conf zshrc gitignore_global vim gvimrc vimrc ssh/config gitconfig
 do
   echo "Linking $file to ~/.$file..";
   ln -sf ~/dotfiles/$file ~/.$file;
