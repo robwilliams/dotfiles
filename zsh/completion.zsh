3e5128375702c67063554e2750698d743aa67de4
Robert Williams <rob@r-williams.com>
2012-05-15 23:08:11 +0100

    Zsh completion case insensitive.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -11,5 +11,6 @@ zstyle ':completion:*' cache-path ~/tmp/zsh/cache
 zstyle ':completion:*:functions' ignored-patterns '_*' # Ignores commands that are not installed.
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:kill:*'   force-list always
-
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' insert-tab pending
 setopt completealiases
