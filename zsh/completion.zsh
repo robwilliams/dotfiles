9d3f1f4dfaad8b62a16447e5a5c544936451f38a
Robert Williams <rob@r-williams.com>
2012-05-16 22:58:13 +0100

    More completion fixes.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,12 +3,23 @@ autoload -U compinit
 compinit
 
 
-zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*:*:*:*:*' menu select
+
+
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/tmp/zsh/cache
+
 zstyle ':completion:*:functions' ignored-patterns '_*' # Ignores commands that are not installed.
+
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:kill:*'   force-list always
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
 zstyle ':completion:*' insert-tab pending
+
 setopt completealiases
