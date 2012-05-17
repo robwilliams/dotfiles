21cd7daacd9a870625b3f9b823a1251147014d7f
Robert Williams <rob@r-williams.com>
2012-05-17 11:11:02 +0100

    Added ackrc

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for file in tmux.conf zsh zshrc gitignore_global vim gvimrc vimrc ssh/config gitconfig gemrc irbrc
+for file in tmux.conf zsh zshrc gitignore_global vim gvimrc vimrc ssh/config gitconfig gemrc irbrc ackrc
 do
   echo "Linking $file to ~/.$file..";
   rm ~/.$file;
