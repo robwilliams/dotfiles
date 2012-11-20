ae6c2968e3917fbb20cc8dfbc881e6b9d2a14b40
Robert Williams <rob@r-williams.com>
2012-11-20 23:33:57 +0000

    Funcs don't work with double quotes. Whoda knew?

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,8 +2,8 @@ setopt auto_name_dirs
 setopt prompt_subst
 
 source $HOME/.zsh/git-prompt/zshrc.sh
-PROMPT="%B%~%b$(git_super_status)
-%# "
+PROMPT='%B%~%b $(git_super_status) 
+%# '
 
 # If we are in a ssh session show the hostname in the right prompt
 #if [[ -n "${SSH_CLIENT+x}" ]]; then
