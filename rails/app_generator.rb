9aefa1498221931d0f351e853118aa69f45c1ba3
Robert Williams <rob@r-williams.com>
2013-02-04 23:20:15 +0000

    Yet test_unit is still in the generators

--- rails/app_generator.rb
+++ rails/app_generator.rb
@@ -6,6 +6,17 @@ gem 'shoulda-matchers', group: :test
 run 'bundle:install'
 generate 'rspec:install'
 
+application do
+  %Q{
+    config.generators do |g|
+      g.test_framework  :rspec, fixture: false
+      g.view_specs      false
+      g.helper_specs    false
+      g.fixture_replacement false
+    end
+  }
+end
+
 remove_dir 'doc'
 remove_file 'README.rdoc'
 remove_file 'public/index.html'
@@ -19,4 +30,3 @@ git add: '.'
 git commit: "-m 'Initial Commit'"
 
 run 'cp config/database.yml.example config/database.yml'
-#rake('db:create')
