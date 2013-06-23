4b7765b73d4cff2348b6d0289155ebd8926fb555
Robert Williams <rob@r-williams.com>
2013-06-23 16:56:24 +0100

    Appending to cdpath instead of project aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,6 +13,7 @@ alias l="ls"
 alias ll="ls -al"
 alias la="ls -al"
 alias tree="tree -C"
+
 alias be="bundle exec "
 alias fr="bundle exec foreman run -e app_env"
 
@@ -20,13 +21,11 @@ alias -g sr=" script/rails "
 alias -g with_tmux=" -t tmux a"
 alias imac_sleep="ssh imac 'pmset sleepnow'"
 
-# Project aliases
-stinkyink=~/Projects/stinkyink/stinkyink
-axis=~/Projects/stinkyink/axis
-steve=~/Projects/stinkyink/steve
-puppet=~/Projects/stinkyink/puppet
-dotfiles=~/dotfiles
-projects=~/Projects
+typeset -gU cdpath
+cdpath=(~/Projects $cdpath)
+cdpath=(~/Projects/stinkyink $cdpath)
+cdpath=(~/Projects/robwilliams $cdpath)
+cdpath=(~/Projects/play $cdpath)
 
 # CHMOD helpers (work just like chmod but only on files or directories)
 chmoddirs () { find $2 -type d -exec chmod $1 {} \; } 
