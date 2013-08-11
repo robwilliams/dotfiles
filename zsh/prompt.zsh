23dd393e662b6291a6853844bf3f531fb90ce32e
Robert Williams <rob@r-williams.com>
2013-08-11 12:19:09 +0100

    Made my prompt more utf8

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,9 @@
 setopt prompt_subst
 
+function ssh_prompt() {
+  if [ $SSH_CONNECTION ]; then echo "%{$fg_bold[white]%}%M⋮"; fi
+}
+
 source $HOME/.zsh/git-prompt/zshrc.sh
-PROMPT='%m:%B%~%b $(git_super_status) 
-%# '
+PROMPT='$(ssh_prompt)%B%~%b $(git_super_status) 
+%#❯ '
