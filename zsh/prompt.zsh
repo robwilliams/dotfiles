2dca520e1a9ccc844aacd44d0060bb785feeda08
Robert Williams <rob@r-williams.com>
2013-08-11 21:29:18 +0100

    More prompt improvements

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,9 @@
 setopt prompt_subst
 
 function ssh_prompt() {
-  if [ $SSH_CONNECTION ]; then echo "%{$fg_bold[white]%}%M⋮"; fi
+  if [ $SSH_CONNECTION ]; then echo "%M⋮"; fi
 }
 
 source $HOME/.zsh/git-prompt/zshrc.sh
 PROMPT='$(ssh_prompt)%B%~%b $(git_super_status) 
-%#❯ '
+%(!.%{$fg[red]%}.)❯ %{${reset_color}%'
