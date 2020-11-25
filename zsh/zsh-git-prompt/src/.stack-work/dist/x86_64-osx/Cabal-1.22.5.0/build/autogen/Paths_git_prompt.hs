151e79dc35eccd5dd4022ea875d1a751c97690ce
Robert Williams <rob@r-williams.com>
2020-11-25 10:43:24 +0000

    Git prompt?

--- zsh/zsh-git-prompt/src/.stack-work/dist/x86_64-osx/Cabal-1.22.5.0/build/autogen/Paths_git_prompt.hs
+++ zsh/zsh-git-prompt/src/.stack-work/dist/x86_64-osx/Cabal-1.22.5.0/build/autogen/Paths_git_prompt.hs
@@ -16,11 +16,11 @@ version :: Version
 version = Version [0,1,0,0] []
 bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/lts-5.0/7.10.3/bin"
-libdir     = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/lts-5.0/7.10.3/lib/x86_64-osx-ghc-7.10.3/git-prompt-0.1.0.0-GyKbGQsRujX2Ik9bjYPmgX"
-datadir    = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/lts-5.0/7.10.3/share/x86_64-osx-ghc-7.10.3/git-prompt-0.1.0.0"
-libexecdir = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/lts-5.0/7.10.3/libexec"
-sysconfdir = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/lts-5.0/7.10.3/etc"
+bindir     = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/330f87550850188dfd78c13ec9865ec5032a7ec2b79a2ee15e384744371b1951/7.10.3/bin"
+libdir     = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/330f87550850188dfd78c13ec9865ec5032a7ec2b79a2ee15e384744371b1951/7.10.3/lib/x86_64-osx-ghc-7.10.3/git-prompt-0.1.0.0-GyKbGQsRujX2Ik9bjYPmgX"
+datadir    = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/330f87550850188dfd78c13ec9865ec5032a7ec2b79a2ee15e384744371b1951/7.10.3/share/x86_64-osx-ghc-7.10.3/git-prompt-0.1.0.0"
+libexecdir = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/330f87550850188dfd78c13ec9865ec5032a7ec2b79a2ee15e384744371b1951/7.10.3/libexec"
+sysconfdir = "/Users/rob/dotfiles/zsh/zsh-git-prompt/.stack-work/install/x86_64-osx/330f87550850188dfd78c13ec9865ec5032a7ec2b79a2ee15e384744371b1951/7.10.3/etc"
 
 getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "git_prompt_bindir") (\_ -> return bindir)
