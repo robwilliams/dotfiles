cdf4b8513b5353f02eb5784c3fc211a8cc3fddbb
Robert Williams <rob@r-williams.com>
2023-02-04 10:24:05 +0000

    Actually got null ls to kinda work

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -54,6 +54,7 @@ require("lazy").setup({
       })
     end,
   },
+  { "RRethy/vim-illuminate" },
   {
     "folke/which-key.nvim",
     config = function()
@@ -183,71 +184,27 @@ require("lazy").setup({
   "vim-test/vim-test",
   -- Language servers
   {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-lua/popup.nvim',
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      'j-hui/fidget.nvim',
-
-      -- Additional lua configuration, makes nvim stuff amazing
-      'folke/neodev.nvim',
-    },
-  },
-  {
-    "folke/neodev.nvim",
-    config = function()
-      require("neodev").setup()
-    end,
-  },
-  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. They will be passed to
-      --  the `settings` field of the server config. You must look up that documentation yourself.
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- tsserver = {},
-
-        sumneko_lua = {
-          Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-          },
-        },
+        "dockerls",
+        -- "eslint-lsp",
+        -- "rome",
+        -- "terraform-ls",
+        -- "tflint",
+        -- "typescript-language-server",
+        "yamlls",
+        -- "yamllint",
+        -- "sumneko_lua"
       }
 
       local mason_lspconfig = require 'mason-lspconfig'
       mason_lspconfig.setup({
-        ensure_installed = vim.tbl_keys(servers),
+        ensure_installed = servers,
       })
 
-      --
-      -- LSP settings.
-      --  This function gets run when an LSP connects to a particular buffer.
       local on_attach = function(_, bufnr)
-        -- NOTE: Remember that lua is a real programming language, and as such it is possible
-        -- to define small helper and utility functions so you don't have to repeat yourself
-        -- many times.
-        --
-        -- In this case, we create a function that lets us more easily define mappings specific
-        -- for LSP related items. It sets the mode, buffer and description for us each time.
         local nmap = function(keys, func, desc)
           if desc then
             desc = 'LSP: ' .. desc
@@ -284,26 +241,152 @@ require("lazy").setup({
         end, { desc = 'Format current buffer with LSP' })
       end
 
-      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      require("neodev").setup(); -- has to be before lspconfig
 
       mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
+        function (server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup({
             on_attach = on_attach,
-            settings = servers[server_name],
-          }
+            capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+          })
+
         end,
       }
     end,
     dependencies = {
-      'neovim/nvim-lspconfig',
-      'telescope.nvim',
-      'hrsh7th/cmp-nvim-lsp',
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "nvim-lua/plenary.nvim",
+      "nvim-lua/popup.nvim",
+      "folke/neodev.nvim",
     },
   },
+
+  -- {
+  --   'neovim/nvim-lspconfig',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-lua/popup.nvim',
+  --     -- Automatically install LSPs to stdpath for neovim
+  --     'williamboman/mason.nvim',
+  --     'williamboman/mason-lspconfig.nvim',
+
+  --     -- Useful status updates for LSP
+  --     'j-hui/fidget.nvim',
+
+  --     -- Additional lua configuration, makes nvim stuff amazing
+  --     'folke/neodev.nvim',
+  --   },
+  -- },
+  -- {
+  --   "folke/neodev.nvim",
+  --   config = function()
+  --     require("neodev").setup()
+  --   end,
+  -- },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   disable = true,
+  --   config = function()
+  --     require("mason").setup()
+  --   end,
+  -- },
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   disable = true,
+  --   config = function()
+  --     -- Enable the following language servers
+  --     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+  --     --
+  --     --  Add any additional override configuration in the following tables. They will be passed to
+  --     --  the `settings` field of the server config. You must look up that documentation yourself.
+  --     local servers = {
+  --       -- clangd = {},
+  --       -- gopls = {},
+  --       -- pyright = {},
+  --       -- rust_analyzer = {},
+  --       -- tsserver = {},
+  --       dockerls = {},
+
+  --       sumneko_lua = {
+  --         Lua = {
+  --           workspace = { checkThirdParty = false },
+  --           telemetry = { enable = false },
+  --         },
+  --       },
+  --     }
+
+  --     local mason_lspconfig = require 'mason-lspconfig'
+  --     mason_lspconfig.setup({
+  --       ensure_installed = vim.tbl_keys(servers),
+  --     })
+
+  --     --
+  --     -- LSP settings.
+  --     --  This function gets run when an LSP connects to a particular buffer.
+       --local on_attach = function(_, bufnr)
+         ---- NOTE: Remember that lua is a real programming language, and as such it is possible
+         ---- to define small helper and utility functions so you don't have to repeat yourself
+         ---- many times.
+         ----
+         ---- In this case, we create a function that lets us more easily define mappings specific
+         ---- for LSP related items. It sets the mode, buffer and description for us each time.
+         --local nmap = function(keys, func, desc)
+           --if desc then
+             --desc = 'LSP: ' .. desc
+           --end
+
+           --vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+         --end
+
+         --nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+         --nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+         --nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+         --nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+         --nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+         --nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+         --nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+         --nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+         ---- See `:help K` for why this keymap
+         --nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+         --nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+         ---- Lesser used LSP functionality
+         --nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+         --nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+         --nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+         --nmap('<leader>wl', function()
+           --print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+         --end, '[W]orkspace [L]ist Folders')
+
+         ---- Create a command `:Format` local to the LSP buffer
+         --vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+           --vim.lsp.buf.format()
+         --end, { desc = 'Format current buffer with LSP' })
+       --end
+
+  --     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+  --     local capabilities = vim.lsp.protocol.make_client_capabilities()
+  --     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+  --     mason_lspconfig.setup_handlers {
+  --       function(server_name)
+  --         require('lspconfig')[server_name].setup {
+  --           capabilities = capabilities,
+  --           on_attach = on_attach,
+  --           settings = servers[server_name],
+  --         }
+  --       end,
+  --     }
+  --   end,
+  --   dependencies = {
+  --     'neovim/nvim-lspconfig',
+  --     'telescope.nvim',
+  --     'hrsh7th/cmp-nvim-lsp',
+  --   },
+  -- },
   -- Shows what nvim-lsp is doing
   {
     "j-hui/fidget.nvim",
@@ -371,23 +454,18 @@ require("lazy").setup({
       }
     end,
     dependencies = { 
-      'hrsh7th/cmp-nvim-lsp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      "zbirenbaum/copilot-cmp",
-      "onsails/lspkind.nvim",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-calc",
       "hrsh7th/cmp-git",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-path",
+      "onsails/lspkind.nvim",
+      "zbirenbaum/copilot-cmp",
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'saadparwaiz1/cmp_luasnip',
     },
   },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-calc" },
-  { "hrsh7th/cmp-git" },
-  { "hrsh7th/cmp-nvim-lua" },
-  { "hrsh7th/cmp-path" },
   { "sindrets/diffview.nvim" },
   { "preservim/nerdcommenter" },
   { "nvim-tree/nvim-web-devicons" },
@@ -480,57 +558,58 @@ require("lazy").setup({
   },
   {
     'jose-elias-alvarez/null-ls.nvim',
+    disable = true,
     config = function()
       local null_ls = require('null-ls');
       null_ls.setup({
         sources = {
-          null_ls.builtins.code_actions.gitrebase,
-          null_ls.builtins.code_actions.gitsigns,
-          null_ls.builtins.completion.spell,
-          null_ls.builtins.diagnostics.ansiblelint,
-          null_ls.builtins.diagnostics.dotenv_linter,
-          null_ls.builtins.diagnostics.erb_lint,
-          null_ls.builtins.diagnostics.eslint_d,
-          null_ls.builtins.diagnostics.fish,
-          null_ls.builtins.diagnostics.gitlint,
-          null_ls.builtins.diagnostics.hadolint,
-          null_ls.builtins.diagnostics.jshint,
-          null_ls.builtins.diagnostics.jsonlint,
-          null_ls.builtins.diagnostics.luacheck,
-          null_ls.builtins.diagnostics.markdownlint,
-          null_ls.builtins.diagnostics.shellcheck,
-          null_ls.builtins.diagnostics.standardrb,
-          null_ls.builtins.diagnostics.stylelint,
-          null_ls.builtins.diagnostics.tidy,
-          null_ls.builtins.diagnostics.trail_space,
-          null_ls.builtins.diagnostics.tsc,
-          null_ls.builtins.diagnostics.vint,
-          null_ls.builtins.diagnostics.write_good,
+          -- null_ls.builtins.code_actions.gitrebase,
+          -- null_ls.builtins.code_actions.gitsigns,
+          -- null_ls.builtins.completion.spell,
+          -- null_ls.builtins.diagnostics.ansiblelint,
+          -- null_ls.builtins.diagnostics.dotenv_linter,
+          -- null_ls.builtins.diagnostics.erb_lint,
+          -- null_ls.builtins.diagnostics.eslint_d,
+          -- null_ls.builtins.diagnostics.fish,
+          -- null_ls.builtins.diagnostics.gitlint,
+          null_ls.builtins.diagnostics.hadolint, -- docker brew install hadolint
+          -- null_ls.builtins.diagnostics.jshint,
+          -- null_ls.builtins.diagnostics.jsonlint,
+          -- null_ls.builtins.diagnostics.luacheck,
+          -- null_ls.builtins.diagnostics.markdownlint,
+          -- null_ls.builtins.diagnostics.shellcheck,
+          -- null_ls.builtins.diagnostics.standardrb,
+          -- null_ls.builtins.diagnostics.stylelint,
+          -- null_ls.builtins.diagnostics.tidy,
+          -- null_ls.builtins.diagnostics.trail_space,
+          -- null_ls.builtins.diagnostics.tsc,
+          -- null_ls.builtins.diagnostics.vint,
+          -- null_ls.builtins.diagnostics.write_good,
           null_ls.builtins.diagnostics.yamllint,
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.beautysh,
-          null_ls.builtins.formatting.cbfmt,
-          null_ls.builtins.formatting.erb_lint,
-          null_ls.builtins.formatting.eslint_d,
-          null_ls.builtins.formatting.fish_indent,
-          null_ls.builtins.formatting.fixjson,
-          null_ls.builtins.formatting.mdformat,
-          null_ls.builtins.formatting.nginx_beautifier,
-          null_ls.builtins.formatting.prettierd,
-          null_ls.builtins.formatting.pg_format,
-          null_ls.builtins.formatting.rubocop,
-          null_ls.builtins.formatting.shellharden,
-          null_ls.builtins.formatting.shfmt,
-          null_ls.builtins.formatting.standardrb,
-          null_ls.builtins.formatting.stylelint,
-          null_ls.builtins.formatting.sqlfluff.with({
-            extra_args = { "--dialect", "postgres" }, -- change to your dialect
-          }),
-          null_ls.builtins.formatting.terraform_fmt,
-          null_ls.builtins.formatting.tidy,
+          -- null_ls.builtins.formatting.stylua,
+          -- null_ls.builtins.formatting.beautysh,
+          -- null_ls.builtins.formatting.cbfmt,
+          -- null_ls.builtins.formatting.erb_lint,
+          -- null_ls.builtins.formatting.eslint_d,
+          -- null_ls.builtins.formatting.fish_indent,
+          -- null_ls.builtins.formatting.fixjson,
+          -- null_ls.builtins.formatting.mdformat,
+          -- null_ls.builtins.formatting.nginx_beautifier,
+          -- null_ls.builtins.formatting.prettierd,
+          -- null_ls.builtins.formatting.pg_format,
+          -- null_ls.builtins.formatting.rubocop,
+          -- null_ls.builtins.formatting.shellharden,
+          -- null_ls.builtins.formatting.shfmt,
+          -- null_ls.builtins.formatting.standardrb,
+          -- null_ls.builtins.formatting.stylelint,
+          -- null_ls.builtins.formatting.sqlfluff.with({
+          --   extra_args = { "--dialect", "postgres" }, -- change to your dialect
+          -- }),
+          -- null_ls.builtins.formatting.terraform_fmt,
+          -- null_ls.builtins.formatting.tidy,
           null_ls.builtins.formatting.trim_whitespace,
-          null_ls.builtins.formatting.yamlfmt,
-          null_ls.builtins.hover.printenv,
+          -- null_ls.builtins.formatting.yamlfmt,
+          -- null_ls.builtins.hover.printenv,
         },
       })
     end,
@@ -538,41 +617,4 @@ require("lazy").setup({
       "nvim-lua/plenary.nvim",
     }
   },
-  { 
-    "folke/trouble.nvim", 
-    config = function()
-      require("trouble").setup {
-        auto_open = false,
-        auto_close = true,
-        auto_preview = false,
-        auto_fold = false,
-        signs = {
-          error = "",
-          warning = "",
-          hint = "",
-          information = "",
-          other = "﫠",
-        },
-        use_diagnostic_signs = true,
-      }
-      vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-        {silent = true, noremap = true}
-      )
-      vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-        {silent = true, noremap = true}
-      )
-      vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-        {silent = true, noremap = true}
-      )
-      vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-        {silent = true, noremap = true}
-      )
-      vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-        {silent = true, noremap = true}
-      )
-      vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-        {silent = true, noremap = true}
-      )
-    end,
-  }
 })
