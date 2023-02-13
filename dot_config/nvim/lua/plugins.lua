b3bb5dbbf15a62ddcfb9db50334c66c73ef171e4
Robert Williams <rob@r-williams.com>
2023-02-13 12:05:25 +0000

    Separate my autocompleting and copiloting
    <C-Space> for copilot even after selecting options in cmp
    <Tab> for cmp, <CR> to choose

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -284,6 +284,13 @@ require("lazy").setup({
     config = function()
       local cmp = require 'cmp'
       local lspkind = require 'lspkind'
+      local luasnip = require 'luasnip'
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       cmp.setup {
         snippet = {
@@ -292,16 +299,33 @@ require("lazy").setup({
           end,
         },
         mapping = {
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+              -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         },
         formatting = {
           format = lspkind.cmp_format({
@@ -352,7 +376,15 @@ require("lazy").setup({
   { "sindrets/diffview.nvim" },
   { "preservim/nerdcommenter" },
   { "nvim-tree/nvim-web-devicons" },
-  { "github/copilot.vim" },
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set('i', '<C-Space>', "copilot#Accept(<Tab>)", { silent = true, expr = true, script = true })
+      --["<C-J>"] = { "copilot#Accept(<Tab>)", silent = true, expr = true, script = true },
+    end,
+
+  },
   {
     'L3MON4D3/LuaSnip',
     config = function()
