0145e7c4f56147d9c0eb7c377b6ada0f47798a43
Robert Williams <rob@r-williams.com>
2025-12-01 12:41:04 +0000

    WIP: add tmux-project-session

--- dot_config/private_fish/conf.d/vscode.fish
+++ dot_config/private_fish/conf.d/vscode.fish
@@ -2,3 +2,10 @@ string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
 
 set -gx EDITOR code
+
+# Auto-attach to persistent tmux session in VSCode
+if string match -q "$TERM_PROGRAM" "vscode"
+    and not set -q TMUX
+    and status is-interactive
+    tmux-project-session
+end
