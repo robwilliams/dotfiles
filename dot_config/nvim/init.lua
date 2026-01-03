710f42f1e525f73c2aaa4be14f54f9bd59c138cf
Robert Williams <rob@r-williams.com>
2026-01-03 21:53:18 +0000

    feat: simplify nvim to minimal config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,26 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Minimal nvim config
+
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.wrap = true
+vim.o.clipboard = ""
+vim.opt.iskeyword:append("-")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.cmd([[
+  cnoreabbrev W! w!
+  cnoreabbrev W1 w!
+  cnoreabbrev w1 w!
+  cnoreabbrev Q! q!
+  cnoreabbrev Q1 q!
+  cnoreabbrev q1 q!
+  cnoreabbrev Wq wq
+  cnoreabbrev WQ wq
+  cnoreabbrev W w
+  cnoreabbrev Q q
+]])
